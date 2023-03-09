c9e046e3558451366d8578eddd47e6717b3ca71d
Akshay7620 <akshayjadhav7620@gmail.com>
2023-03-09 18:07:13 +0530

    crud

---
+++ Application_crud/Migrations/202303090921216_crud1.cs
@@ -0,0 +1,17 @@
+﻿namespace Application_crud.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+
+    public partial class crud1 : DbMigration
+    {
+        public override void Up()
+        {
+
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
